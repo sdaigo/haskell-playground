fe9ae30c674434aa2932ad6f84ff8ce4c6595620
Daigo Shitara <stain.witness+github@gmail.com>
2022-09-05 09:44:31 +0900

    pattern match / gurard

--- guard.hs
+++ guard.hs
@@ -1,9 +1,39 @@
 bmiTell :: Double -> Double -> String
+-- bmiTell weight height
+--   | weight / height ^ 2 <= 18.5 = "You're underweight"
+--   | weight / height ^ 2 <= 25.0 = "You're supposedly normal"
+--   | weight / height ^ 2 <= 30.0 = "You're fat"
+--   | otherwise = "You're a whale, congraturations!"
 bmiTell weight height
-  | weight / height ^ 2 <= 18.5 = "You're underweight"
-  | weight / height ^ 2 <= 25.0 = "You're supposedly normal"
-  | weight / height ^ 2 <= 30.0 = "You're fat"
+  | bmi <= skinny = "You're underweight"
+  | bmi <= normal = "You're supposedly normal"
+  | bmi <= fat = "You're fat"
   | otherwise = "You're a whale, congraturations!"
+  where
+    bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25.0
+    fat = 30.0
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi weight height = weight / height ^ 2
+
+greet :: String -> String
+-- greet "Juan" = niceGreeting ++ "Juan!"
+-- greet "Fernand" = niceGreeting ++ "Fernand!"
+-- greet name = badGreeting ++ " " ++ name
+--   where
+--     niceGreeting = "Hello! So very nice to see you,"
+--     badGreeting = "Oh! Pfft. It's you."
+greet name
+  | name == "Juan" = niceGreeting ++ "Juan!"
+  | name == "Fernand" = niceGreeting ++ "Fernand!"
+  | otherwise = badGreeting ++ " " ++ name
+  where
+    niceGreeting = "Hello! So very nice to see you,"
+    badGreeting = "Oh! Pfft. It's you."
 
 max' :: (Ord a) => a -> a -> a
 max' a b
--- pattern-match.hs
+++ pattern-match.hs
@@ -20,3 +20,30 @@ addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 firstLetter :: String -> String
 firstLetter "" = "Empty string!"
 firstLetter all@(x : _) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- let式
+cylinder :: Double -> Double -> Double
+cylinder r h =
+  let side = 2 * pi * r * h
+      top = pi * r ^ 2
+   in side + 2 * top
+
+calcBmi :: [(Double, Double)] -> [Double]
+calcBmi xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+-- case式
+head' :: [a] -> a
+head' [] = error "No head for empty list"
+head' (x : _) = x
+
+head'' :: [a] -> a
+head'' xs = case xs of
+  [] -> error "No head for empty list"
+  (x : _) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ what ls
+  where
+    what [] = "empty."
+    what [x] = "a singleton list."
+    what xs = "a longer list."
